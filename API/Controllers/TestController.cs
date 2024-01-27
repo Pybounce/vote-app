13dbe34295a5e2e2776c3146607c17d4f1315dff
Pybounce <pybounce@Pybounces-MacBook-Pro.local>
2024-01-27 23:15:38 +0000

    initial api

---
+++ API/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+
+public class TestController : Controller
+{
+    [HttpGet("HelloWorld")]
+    public string HelloWorld()
+    {
+        return "Hello World!";
+    }
+}
+
+
